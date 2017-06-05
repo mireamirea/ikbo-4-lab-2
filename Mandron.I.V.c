edb0aca749af066d24359a540ef36cc82b8de10e
shovut <tbikba4ok@mail.ru>
2017-06-05 16:19:20 +0300

    Update Mandron.I.V.c
    Все Коментарии не стерты исключительно ради меня и виндовс.

--- Mandron.I.V.c
+++ Mandron.I.V.c
@@ -1,14 +1,13 @@
-﻿#include <stdio.h> 
+#include <stdio.h> 
 #include <stdlib.h> 
 #include <string.h> 
-#include <conio.h> 
 
 int main(int argc, char **argv) {
 
 	FILE *fp;
 	char name[255];
 	int x, i = 0, size=256, sizenew, g, j, temp=0, iter=0, temp2;
-	char *a = NULL, *b;
+	char *a = NULL, tempa, tempb;
 
 	char *lent;
 
@@ -20,7 +19,7 @@ int main(int argc, char **argv) {
 	while (fgets(name, sizeof(name), fp)) {
 
 		
-		system("cls");
+/*		system("cls");*/
 		for (g = 0; g < i + 5; g++) printf("%c", lent[g]);
 		printf("\n");
 		for (g = 0; g < i; g++) printf(" ");
@@ -31,16 +30,16 @@ int main(int argc, char **argv) {
 
 
 		switch (x) {
-//------------------------------------------------------------------------------------------------
+/*------------------------------------------------------------------------------------------------*/
 		case 0:
 			if (i == 0) {
 				printf("Left border reached\nExpansion is not possible\n");
-				_sleep(3000);
+/*				_sleep(3000);*/
 			}
 			else
 				i--;
 			break;
-//------------------------------------------------------------------------------------------------
+/*------------------------------------------------------------------------------------------------*/
 		case 1:
 			if (i == size - 1) {
 				sizenew += size;
@@ -48,7 +47,7 @@ int main(int argc, char **argv) {
 				if (a == NULL) {
 					printf("Unable to expand the ribbon\n");
 					sizenew -= size;
-					_sleep(3000);
+/*					_sleep(3000);*/
 					break;
 				}
 				else {
@@ -58,52 +57,52 @@ int main(int argc, char **argv) {
 			}
 			i++;
 			break;
-//------------------------------------------------------------------------------------------------
+/*------------------------------------------------------------------------------------------------*/
 		case 2:
 			if (lent[i] == 255) {
 				printf("Overflow\nCell is reset to zero\n");
 				lent[i] = 0;
-				_sleep(3000);
+/*				_sleep(3000);*/
 			}
 			else {
 				lent[i]++;
-				_sleep(100);
+/*				_sleep(100);*/
 			}
 			break;
-//------------------------------------------------------------------------------------------------
+/*------------------------------------------------------------------------------------------------*/
 		case 3:
 			if (lent[i] == 0) {
 				printf("The cell has a minimum value\n");
-				_sleep(3000);
+/*				_sleep(3000);*/
 			}
 			else {
 				lent[i]--;
-				_sleep(100);
+/*				_sleep(100);*/
 			}
 			break;
-//------------------------------------------------------------------------------------------------
+/*------------------------------------------------------------------------------------------------*/
 		case 4:
-			printf("The value in the current cell: %d\n", lent[i]);
-			_sleep(3000);
+			printf("The value in the current cell: %d\n", (int)lent[i]);
+/*			_sleep(3000);*/
 			break;
-//------------------------------------------------------------------------------------------------
+/*------------------------------------------------------------------------------------------------*/
 		case 5:
 			printf("Enter the current cell symbol\n");
-			scanf("%c", &a);
-			if(a!=10) scanf("%c", &b);
-			while (a < 0 || a>255) {
+			scanf("%c", &tempa);
+			if((int)tempa!=10) scanf("%c", &tempb);
+			while ((int)tempa < 0 || (int)tempa>255) {
 				printf("Invalid character\nTry another character\n");
-				scanf("%c", &a);
-				if (a != 10) scanf("%c", &b);
+				scanf("%c", &tempa);
+				if ((int)tempa != 10) scanf("%c", &tempb);
 			}
-			lent[i] = a;
+			lent[i] = tempa;
 			break;
-//------------------------------------------------------------------------------------------------
+/*------------------------------------------------------------------------------------------------*/
 		case 6:
 			printf("The character in the current cell: %c\n", lent[i]);
-			_sleep(3000);
+/*			_sleep(3000);*/
 			break;
-//------------------------------------------------------------------------------------------------
+/*------------------------------------------------------------------------------------------------*/
 		case 7:
 			temp = 1;
 			iter = ftell(fp);
@@ -112,15 +111,15 @@ int main(int argc, char **argv) {
 				fgets(name, sizeof(name), fp);
 				iter++;
 				j = dei(name);
-				if (j == NULL) {
+				if (j == 0) {
 					printf("The instruction does not include the command \"end\"\n");
 					fseek(fp, iter, SEEK_SET);				
-					_sleep(3000);
+/*					_sleep(3000);*/
 				}
 			}
 		}
 			break;
-//------------------------------------------------------------------------------------------------
+/*------------------------------------------------------------------------------------------------*/
 		case 8:
 			if (temp == 1) {
 				if (lent[i] != 0) {
@@ -129,52 +128,51 @@ int main(int argc, char **argv) {
 			}
 			else {
 				printf("In the instruction there is no command \"begin\"\n");
-				_sleep(3000);
+/*				_sleep(3000);*/
 			}
 			break;
-//------------------------------------------------------------------------------------------------
+/*------------------------------------------------------------------------------------------------*/
 		case 9:
 			printf("Commentary:\n");
-			while (name[0] == " " || name[0] == "\t") {
+			while (name[0] == ' ' || name[0] == '\t') {
 				for (j = 1; j < 255; j++) name[j - 1] = name[j];
 			}
 			printf("%s\n", name);
-			_sleep(3000);
+/*			_sleep(3000);*/
 			break;
 		}
-		
+		printf("--------------------------------------------\n");
 
 	}
 
 	fclose(fp);
-	system("pause");
+	getchar();
 
 }
 
 int dei(char st[255]) {
 	int i, g, h;
-	char *s[] = {
-		{ "movl" },
-		{ "movr" },
-		{ "inc" },
-		{ "dec" },
-		{ "print" },
-		{ "get" },
-		{ "printc" },
-		{ "begin" },
-		{ "end" },
-		{ "*" },
-	};
+	char s0[] ="movl";
+	char s1[] ="movr";
+	char s2[] ="inc";
+	char s3[] ="dec";
+	char s4[] ="print";
+	char s5[] ="get";
+	char s6[] ="printc";
+	char s7[] = "begin";
+	char s8[] = "end";
+	char s9[] = "*";
+	
 
-	if (strstr(st, s[0]) != NULL) { return 0; };
-	if (strstr(st, s[1]) != NULL) { return 1; };
-	if (strstr(st, s[2]) != NULL) { return 2; };
-	if (strstr(st, s[3]) != NULL) { return 3; };
-	if (strstr(st, s[6]) != NULL) { return 6; };
-	if (strstr(st, s[5]) != NULL) { return 5; };
-	if (strstr(st, s[4]) != NULL) { return 4; };
-	if (strstr(st, s[7]) != NULL) { return 7; };
-	if (strstr(st, s[8]) != NULL) { return 8; };
-	if (strstr(st, s[9]) != NULL) { return 9; };
+	if (strstr(st, s0) != NULL) { return 0; };
+	if (strstr(st, s1) != NULL) { return 1; };
+	if (strstr(st, s2) != NULL) { return 2; };
+	if (strstr(st, s3) != NULL) { return 3; };
+	if (strstr(st, s6) != NULL) { return 6; };
+	if (strstr(st, s5) != NULL) { return 5; };
+	if (strstr(st, s4) != NULL) { return 4; };
+	if (strstr(st, s7) != NULL) { return 7; };
+	if (strstr(st, s8) != NULL) { return 8; };
+	if (strstr(st, s9) != NULL) { return 9; };
 }
-//printf("GG\n");
+/*printf("GG\n");*/
