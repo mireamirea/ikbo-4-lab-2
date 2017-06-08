fe4038bf6b7ada4b0fafe73712938a89d7662dbd
MarMaxim <maksim.marchenko.1997@mail.ru>
2017-06-08 11:32:43 +0300

    Add files via upload

--- Марченко/2.c
+++ Марченко/2.c
@@ -2,97 +2,83 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-int loc = 0;
-int *tape;
-int sizeOfLenta = 1;
+typedef struct
+{
+    int loc;
+    int *tape;
+    int size;
+}machine;
 
 
 
 
-int movr(void){
-    if (loc == (sizeOfLenta - 1)){
-        int* temp;
-        sizeOfLenta++;
-        loc++;
-        temp = (int*) realloc (tape, sizeof(int) * sizeOfLenta);
-        if(temp != NULL){
-            temp[sizeOfLenta-1] = 0;
-            tape = temp;
+
+machine movr(machine temp)
+{
+    int i;
+    machine newTemp;
+    if (temp.loc == (temp.size - 1)){
+        newTemp.size=temp.size+1;
+        newTemp.loc=temp.loc+1;
+        newTemp.tape=(int*)malloc(sizeof(int) * newTemp.size);
+        for(i=0;i<temp.size;i++)
+        {
+            newTemp.tape[i]=temp.tape[i];
         }
-        else
-            return 1;
-    }
-    else loc++;
-return 0;
-}
+        newTemp.tape[newTemp.size-1] = 0;
 
-int movl(void){
-    if (loc == 0){
-        int i;
-        int* temp;
-        sizeOfLenta++;
-        temp = (int*) realloc (tape, sizeof(int) * sizeOfLenta);
-        if(temp != NULL){
-            for (i = sizeOfLenta - 1; i != 0; i--)
-                temp[i]=temp[i-1];
-            temp[0] = 0;
-            tape = temp;
-        }
-        else
-            return 1;
     }
-    else loc--;
-return 0;
-}
-
-int inc (void) {
-    if(tape[loc] < 255)
-        tape[loc]++;
     else
-        tape[loc] = 0;
-return 0;
-}
-
-int dec (void) {
-    if(tape[loc] > 0)
-        tape[loc]--;
-    else
-        tape[loc] = 255;
-return 0;
-}
-
-int doCommands (char** a, int first, int last){
-    int i, tmp = 0;
-    for (i = first; i < last; i++){
-        if (strcmp(a[i], "movr") == 0)
-            movr();
-        else if (strcmp(a[i], "movl") == 0)
-            movl();
-        else if (strcmp(a[i], "inc") == 0)
-            inc();
-        else if (strcmp(a[i], "dec") == 0)
-            dec();
-        else if (strcmp(a[i], "print") == 0)
-            printf("\n%d", tape[loc]);
-        else if (strcmp(a[i], "get") == 0){
-                printf("\nEnter symbol:");
-            tape[loc] = getchar();
-        }
-        else if (strcmp(a[i], "printc") == 0)
-            printf("\n%c", tape[loc]);
-        else if (strcmp(a[i], "begin") == 0)
-             tmp=i+1;
-        else if (strcmp(a[i], "end") == 0)
-            {
-                while (tape[loc] != 0)
-                    doCommands(a, tmp, i);
-            }
-        else
-            printf("\nerror %s",a[i]);
+    {
+        newTemp=temp;
+        newTemp.loc=temp.loc+1;
     }
-
-return 0;
+    return newTemp;
 }
+
+
+machine movl(machine temp)
+{
+    int i;
+    machine newTemp;
+    if (temp.loc == 0)
+    {
+        newTemp.size=temp.size+1;
+        newTemp.tape=(int*)malloc(sizeof(int) * newTemp.size);
+        for (i=0;i<temp.size;i++)
+            newTemp.tape[i+1]=temp.tape[i];
+        newTemp.tape[0]=0;
+
+    }
+    else
+    {
+        newTemp=temp;
+        newTemp.loc=temp.loc-1;
+    }
+return newTemp;
+}
+
+machine inc (machine temp) {
+    machine newTemp;
+    newTemp=temp;
+    if(temp.tape[temp.loc] < 255)
+        newTemp.tape[temp.loc]=temp.tape[temp.loc]+1;
+    else
+        newTemp.tape[temp.loc] = 0;
+    return newTemp;
+}
+
+machine dec (machine temp) {
+    machine newTemp;
+    newTemp=temp;
+    if(temp.tape[temp.loc] > 0)
+        newTemp.tape[temp.loc]=temp.tape[temp.loc]-1;
+    else
+        newTemp.tape[temp.loc] = 255;
+    return newTemp;
+}
+
+
 
 char* delSpace (char* s){
     char* a;
@@ -104,8 +90,54 @@ char* delSpace (char* s){
     a[j] = '\0';
     return a;
 }
+machine doCommands (char** a, int first, int last, machine temp)
+{
+    machine newTemp;
+    newTemp=temp;
+    int i, beginPosition = 0;
+    for (i = first; i < last; i++){
+        if (!strcmp(a[i], "movr"))
+            newTemp=movr(newTemp);
+        else if (!strcmp(a[i], "movl"))
+            newTemp=movl(newTemp);
+        else if (!strcmp(a[i], "inc"))
+            newTemp=inc(newTemp);
+        else if (!strcmp(a[i], "dec"))
+            newTemp=dec(newTemp);
+        else if (!strcmp(a[i], "print"))
+            printf("\n%d", newTemp.tape[newTemp.loc]);
+        else if (!strcmp(a[i], "get")){
+                printf("\nInput symbol:");
+            newTemp.tape[newTemp.loc] = getchar();
+        }
+        else if (!strcmp(a[i], "printc"))
+            printf("\n%c", newTemp.tape[newTemp.loc]);
+        else if (!strcmp(a[i], "begin"))
+             beginPosition=i;
+        else if (!strcmp(a[i], "end"))
+            {
+                while (newTemp.tape[newTemp.loc] != 0)
+                    newTemp=doCommands(a, beginPosition+1, i,newTemp);
+            }
+        else
+            printf("\nerror %s",a[i]);
+    }
+
+return newTemp;
+}
+int printTape(machine temp)
+{
+    int i;
+    printf("\nTape:");
+    for(i = 0; i < temp.size; i++)
+        printf("  %d", temp.tape[i]);
+    return 0;
+}
 
 int main(){
+    machine myMachine;
+    myMachine.loc=0;
+    myMachine.size=1;
     int num = 0, i = 0;
     char s[255];
     char **commands;
@@ -114,18 +146,17 @@ int main(){
     FILE *f;
     f = fopen("test.txt", "r");
     while (f == NULL){
-        printf(" Error opening file\n Enter a new filename");
+        printf("File test.txt not found\n Enter a new filename: ");
         scanf("%s", fname);
         f=fopen(fname, "r");
     }
 
     commands=(char **) malloc (sizeof(char*));
     while(fgets(s, 255, f)){
-
         if(s[0] != '*'){
-            commands[num]=(char *) malloc (100*sizeof(char));
-            commands[num] = delSpace(s);
 
+            commands[num]=(char *) malloc (100*sizeof(char));
+            commands[num]=delSpace(s);;
 
             commands = (char**) realloc (commands,(num+2)*sizeof(char*));
             num++;
@@ -133,15 +164,13 @@ int main(){
     }
     fclose(f);
 
-    tape = (int*) malloc (sizeof(int));
-    tape[loc]=0;
-    doCommands(commands, 0, num);
+    myMachine.tape = (int*) malloc (sizeof(int));
+    myMachine.tape[myMachine.loc]=0;
+    myMachine=doCommands(commands, 0, num,myMachine);
 
     for(i = 0; i < num; i++)
         free(commands[num]);
     free(commands);
-    printf("\nLenta : ");
-    for(i = 0; i < sizeOfLenta; i++)
-        printf("  %d", tape[i]);
+    printTape(myMachine);
     return 0;
 }
